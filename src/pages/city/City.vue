88bcd839180670252aa764406a21961a745239a7
muxiaomei <2697694242@qq.com>
2022-08-31 10:18:09 +0800

    city-router

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,56 +1,16 @@
 <template>
 	<div>
-		<city-header></city-header>
-		<city-search :cities="cities"></city-search>
-		<city-list :cities="cities" :hot="hotCities" :letter="letter"></city-list>
-		<city-alphabet :cities="cities" @change="handleLetterChange"></city-alphabet>
+   
+     <city-header></city-header>
 	</div>
 </template>
 
 <script>
-import axios from 'axios'
-import CityHeader from './components/Header.vue'
-import CitySearch from './components/Search.vue'
-import CityList from './components/List.vue'
-import CityAlphabet from './components/Alphabet.vue'
+import CityHeader from './components/Header.vue';
 	export default {
-		name:"City",
-		components:{
-			CityHeader,
-			CitySearch,
-            CityList,
-			CityAlphabet
-		},
-		data(){
-			return {
-				cities:{},
-				hotCities:[],
-				letter:''
-			}
-		},
-		methods:{
-			getCityInfo(){
-				axios.get('/api/city.json')
-				  .then(this.handleGetCityInfoSucc)
-			},
-			handleGetCityInfoSucc(res){
-				res=res.data
-				console.log(res)
-				if(res.ret  && res.data){
-					const data=res.data
-					console.log(data.hotCities)
-					this.cities=data.cities
-					this.hotCities=data.hotCities
-				}
-			},
-			handleLetterChange(letter){
-				this.letter=letter
-			}
-		},
-		mounted(){
-			this.getCityInfo()
-		}
-	}
+    name: "MyCity",
+    components: { CityHeader }
+}
 </script>
 
 <style lang="stylus" scoped>
