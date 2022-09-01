610eb06990ce1db11e7e7d67b64ef87e1829cd55
muxiaomei <2697694242@qq.com>
2022-09-01 14:38:38 +0800

    city-ajax

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,19 +2,44 @@
 	<div>
 		<city-header></city-header>
 		<city-search></city-search>
-		<city-list></city-list>
-    <city-alphabet></city-alphabet>
+		<city-list :cities="cities" :hot="hotCities"></city-list>
+    <city-alphabet :cities="cities"></city-alphabet>
 	</div>
 </template>
 
 <script>
+import axios from 'axios'
 import CityHeader from './components/Header.vue';
 import CitySearch from './components/Search.vue';
 import CityList from './components/List.vue';
 import CityAlphabet from "./components/Alphabet.vue";
 export default {
 	name: "MyCity",
-	components: {CityAlphabet, CityHeader, CitySearch, CityList }
+  data(){
+    return{
+      cities:{},
+      hotCities:[],
+    }
+  },
+	components: {CityAlphabet, CityHeader, CitySearch, CityList },
+  methods:{
+    getCityInfo(){
+      axios.get('/api/city.json')
+      .then(this.handleGetCityInfoSucc)
+    },
+    handleGetCityInfoSucc(res){
+      // console.log(res)
+    res=res.data
+      if(res.ret &&res.data){
+        const data=res.data
+        this.cities=data.cities
+        this.hotCities=data.hotCities
+      }
+    }
+  },
+  mounted() {
+    this.getCityInfo()
+  }
 }
 </script>
 
--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,13 +1,7 @@
 <template>
   <ul class="list">
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
+    <li class="item" v-for="(item,key) of cities" :key="key">{{key}}</li>
+
 
 
 
@@ -16,6 +10,9 @@
 <script>
 export default {
   name:'CityAlphabet',
+  props:{
+    cities:Object
+  }
 
 }
 </script>
