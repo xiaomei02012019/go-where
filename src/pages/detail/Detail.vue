43845c543b28dd654d74bb623107ef84a3417511
muxiaomei <2697694242@qq.com>
2022-09-02 23:48:55 +0800

    detail-header

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -3,7 +3,7 @@
 		<detail-banner></detail-banner>
 		<detail-header></detail-header>
 		<div class="content">
-			<detail-list :list="list"></detail-list>
+		
 		</div>
 	</div>
 </template>
@@ -11,40 +11,15 @@
 <script>
     import DetailBanner from './componments/Banner.vue'
 	import DetailHeader from './componments/Header.vue'
-	import DetailList from './componments/List.vue'
-	import axios from 'axios'
+
 	export default {
 		name:'MyDetail',
 		components:{
 			DetailBanner,
 			DetailHeader,
-			DetailList
+		
 		},
-		data(){
-			return{
-				list:[{
-				title:'成人票',
-				children:[{
-					title:'成人三馆联票',
-					children:[{
-					title:'成人三馆联票-某一连锁店销售'
-				}]
-				},{
-					title:'成人五馆联票'
-				}]
-			},{
-				title:'学生票'
-			},{
-				title:'儿童票'
-			},{
-				title:'特惠票'
-			},]}
-		},
-		methods:{
-			getDetailInfo(){
-                 axios.get('/api/detail/json?id='+this.$route.params.id)
-			}
-		},
+		
 		mounted(){
 			this.getDetailInfo()
 		}
@@ -52,4 +27,6 @@
 </script>
 
 <style lang="stylus" scoped>
+.content 
+  height: 50rem
 </style>
